40623b314f96c628393537fb0bab0ecb37878f98
Justin Legros <justinlegros@android-f9d229577baed6ec.localdomain>
2022-08-03 17:39:27 -0700

    initial website

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new fullpage('#fullpage', {
+    anchors: ['home','artists','merch','tour'],
+    autoScrolling:true,
+    scrollHorizontally: true,
+    menu: '#menu',
+    verticalCentered: true,
+    fixedElements: ['.socials', '#header','#menu']
+
+});
+fullpage_api.setAllowScrolling(true);
+
---
+++ js/index.js
@@ -0,0 +1,3 @@
+import fullpage from './app';
+
+fullpage();
